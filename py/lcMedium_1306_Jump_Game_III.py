55edd6b14a625d3ad3612ad8e85b517c1804e08d
StanS <57524448+stansuo@users.noreply.github.com>
2020-12-01 14:58:34 +0800

    Add files via upload
    lcMedium_1306_Jump_Game_III.py

---
+++ py/lcMedium_1306_Jump_Game_III.py
@@ -0,0 +1,64 @@
+# index = i
+# value = v (i.e., arr[i])
+# winning: reach the index with value 0
+# action: frontier
+#   forward_step(FS) = v
+#   backward_step(BS) = -v
+# rules: can not jump outside of the array
+#        (i.e., arr[FS] <= len(arr), arr[BS]>= 0)
+# planing: 
+#     1. frontier, if empty, return True
+#     2. visited, for route tracing->use list; 
+#                 for checking visited->use set. 
+#     3. pop from frontier and append visited. check visited each time.
+class Solution:
+    def document_it(func):
+        '''
+        Used as Decorator to examine input and output of specific function(s)
+        '''
+        def documented_func(*args, **kwargs):
+            print('Running function:', func.__name__)
+            print('Positional arguments:', args)
+            print('Keyword arguments:', kwargs)
+            result = func(*args, **kwargs)
+            print('Result:', result)
+            print('===========')
+            return result
+        return documented_func
+
+    @document_it
+    def canReach(self, arr: list, start: int) -> bool:               
+        visited = []
+        def walker(i):
+            '''
+            take in an index and return next steps
+            any of the route reach winning condition will return True
+            '''
+            # Check index valid
+            if i < 0 or i >= len(arr):
+                return False            
+            
+            # Check if win
+            if arr[i] == 0:
+                return True 
+
+            # Check if visited 
+            # (means this route is an endless loop which will not win)
+            if i in visited:
+                return False
+                        
+            visited.append(i)
+            print(visited)
+            return walker(i + arr[i]) or walker(i - arr[i])
+        
+        return walker(start)
+
+
+sol1 = Solution()
+sol1.canReach(arr = [4,2,3,0,3,1,2], start = 5)
+        
+sol2 = Solution()
+sol2.canReach(arr = [4,2,3,0,3,1,2], start = 0)
+
+sol3 = Solution()
+sol3.canReach(arr = [3,0,2,1,2], start = 2)
